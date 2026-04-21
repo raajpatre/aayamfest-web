e9bdd277f3d0e5989c7a65175e802886975cc2c0
raajpatre <raajpatre27@gmail.com>
2026-04-21 09:40:20 +0530

    corrections

--- components/ui/WarpSpeedIntro.tsx
+++ components/ui/WarpSpeedIntro.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
+import Galaxy from "@/components/Galaxy";
 import { usePerformance } from "@/lib/hooks/usePerformance";
 import styles from "./WarpSpeedIntro.module.css";
 
